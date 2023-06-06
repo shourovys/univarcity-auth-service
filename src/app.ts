1141b5b9fec80ba54725a026e99eb6319f78f603
Shourov Saha <shourovys@gmail.com>
2023-06-06 10:57:16 +0600

    setup eslint prettier husky lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import cors from "cors";
-import express, { Application } from "express";
+import cors from 'cors'
+import express, { Application } from 'express'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import confirm from "./config/index";
-
-const port = 3000;
+import mongoose from 'mongoose'
+import app from './app'
+import confirm from './config/index'
 
 async function main() {
   try {
-    await mongoose.connect(confirm.database_url as string);
-    console.log("DB connect successful");
+    await mongoose.connect(confirm.database_url as string)
+    console.log('DB connect successful')
 
     app.listen(confirm.port, () => {
-      console.log(`Application listening on port ${confirm.port}`);
-    });
+      console.log(`Application listening on port ${confirm.port}`)
+    })
   } catch (error) {
-    console.log("DB not connected", error);
+    console.log('DB not connected', error)
   }
 }
-main();
+main()
